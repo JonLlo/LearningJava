c1237df2b00decd86396ded7fe808ecb5df08e52
jonnylloyd <jonnylloydie2@gmail.com>
2025-11-20 15:02:44 +0000

    Lesson 6 completed

--- src/Main.java
+++ src/Main.java
@@ -69,7 +69,7 @@ void main() {
     //note number2 is the same as the binary 0110 1101 above (*)
     IO.println(number2 * 2);
 
-    //Lesson 4: string class
+    //Lesson 4/5: string class
 
     String caveman1 = "Fred Flintstone";
     String caveman2 = new String ("Fred Flintstone"); //allows for 2 fred flints in the heap
@@ -96,6 +96,7 @@ void main() {
     IO.println(multiline);
 
     //scanner
+    /*
     Scanner reader =  new Scanner(System.in);
     IO.println("What is your name?: ");
     String yourName = reader.nextLine().trim();
@@ -104,8 +105,99 @@ void main() {
     //yourAge = yourAge + (byte)1; cannot cast here
     yourAge++;
     System.out.printf("Your name is %s, and your age next year will be %d", yourName, yourAge);
+    */
+    //
+    Integer type1 = 128; //autoboxing (int to Integer)
+    Integer type2 = 128;
+    IO.println(type1 == type2); //this is false
+    IO.println(type1.equals(type2)); //this is true
 
-    
+    Byte type3 = 127;
+    Byte type4 = 127;
+    IO.println(type3 == type4); //this is true
+    IO.println(type3.equals(type4)); //this is true
+
+    short type5 = 128;
+    short type6 = 128;
+    IO.println(type5 == type6);
+    //IO.println(type5.equals(type6)); doesn't work as short is primitive and
+    // primitive cannot use .equals - only wrapper can.
+
+//lesson 6: casting
+
+    //byte --> short --> int ->> long -> float -> double (left can easily transfer to right) (WIDENESS)
+    //long --> int --> short --> byte (even if number range is inside group, still cant transfer)
+    long longNum = 100;
+    //int intNum = longNum;no work must cast:
+    int intNum = (int)longNum;
+
+    int numInt1 = 2;
+    short numShort2 = 3;
+    float result = numInt1/numShort2;
+    long numLong3 = 51617228l;
+    double result1 = numInt1 + numLong3; //WIDENING CONVERSION (as double is wider than above)
+    //right hand needs to match the deepest type on the left (i.e. WIDENESS above^^)
+    IO.println(result1);
+
+    //NARROWING CONVERSION:
+    double price = 1.49; //litre price for petrol
+    int capacity = 20; //in litres
+    int totalCostToFillTankFromEmpty =  capacity * (int) price; //NARROWING CONVERSION!!!!
+    double totalCostToFillTankFromEmptyDouble = capacity * price;
+    IO.println(totalCostToFillTankFromEmpty); //narrowing
+    IO.println(totalCostToFillTankFromEmptyDouble); //not narrowing
+
+    //casting between numbers and letters
+    char letter = '?';
+    int giveMeTheNumber = (int) letter; //65 - on ASCII table
+    IO.println(giveMeTheNumber);
+
+    int letterValue = 97;
+    char thisIsTheCharacter = (char) letterValue;
+    IO.println(thisIsTheCharacter);
+//won't use this below bit again but shows you how it works//
+    char digit = '7';
+    int value = digit - '1';
+    IO.println(value);
+//
+    char letter1 = 'B';
+    char newLetter = (char)(letter1 + 32);
+    IO.println(newLetter);
+    //final
+    final int MAX_RETRIES = 5; //naming convention for final is uppercase + camelcase
+    //MAX_RETRIES = 6; -> no possible
+
+    //stack v heap:
+    //stack is an ordered area.
+
+
+IO.println(3+6*7/9.0f); //prints float as float value is the widest
+
+    //unary assignments
+    byte b1 = -1;
+    byte b2 = b1++;
+    IO.println(b2);
+    IO.println(b1);
+    IO.println(-b2);
+
+    IO.println(1/2.0); //double is the widest and so 0.5 outputs;
+    IO.println(1/2); // int is the widest so 0 outputs;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
